21cae113252a5788ccd8ecea5a9798d7ab2ec48c
Joonas Laukka <joonas.laukka@relexsolutions.com>
2023-03-06 14:51:43 +0200

    Pin nixpkgs for shell.nix

---
+++ nix/nixpkgs.pin.nix
@@ -0,0 +1,5 @@
+fetchTarball {
+  # Branch & commit date: release-22.11 2023-03-02
+  url = "https://github.com/NixOS/nixpkgs/archive/96e18717904dfedcd884541e5a92bf9ff632cf39.tar.gz";
+  sha256 = "0zw1851mia86xqxdf8jgy1c6fm5lqw4rncv7v2lwxar3vhpn6c78";
+}
--- shell.nix
+++ shell.nix
@@ -1,7 +1,9 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    nativeBuildInputs = [ 
-      pkgs.nodePackages.create-react-app
-      pkgs.nodejs
-    ];
+{ pkgs ? import (import nix/nixpkgs.pin.nix) {}
+}:
+
+pkgs.mkShell {
+  nativeBuildInputs = [
+    pkgs.nodePackages.create-react-app
+    pkgs.nodejs
+  ];
 }
